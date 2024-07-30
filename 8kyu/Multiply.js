3596d8ba9a05707896890a83450da30900496a63
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-30 16:30:59 +0300

    Multiply

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+// SOLUTION:
+let i = 0
+function multiply(a, b) {
+ i = a * b;
+ return i
+}
+multiply(2, 3)
