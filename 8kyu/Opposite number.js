8d4bd19773a5fa4d4817899e8cd914da48fbab63
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-03 12:06:19 +0300

    Opposite_number

---
+++ 8kyu/Opposite number.js
@@ -0,0 +1,14 @@
+// ❓ DESCRIPTION:
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// ❗ Solutions
+
+function opposite(number) {
+    return number * -1
+  }
