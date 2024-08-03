f9720677c8cdcf2541daeda3f5311181d6a4831c
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-03 21:17:53 +0300

    Even or Odd

---
+++ 8kyu/Even or Odd.js
@@ -0,0 +1,10 @@
+// ❓DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+// ❗ Solutions
+
+function evenOrOdd(number) {
+    return (number % 2) ? "Odd" : "Even"
+    }
+    console.log(evenOrOdd(1));
