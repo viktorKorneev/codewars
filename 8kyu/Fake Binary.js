d26ddb4fac38a6ba4150e05aacbc500a54a737d6
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-08 00:59:05 +0300

    Fake Binary

---
+++ 8kyu/Fake Binary.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// ❗ Solutions
+
+function fakeBin(x) {
+    let result = "";
+  
+    x.split("").forEach((el) => {
+      result += el < 5 ? "0" : "1";
+    });
+  
+    return result;
+  }
