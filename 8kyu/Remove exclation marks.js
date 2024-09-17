a41611df6283695bfdb9e07fe9dba4428398576b
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-17 20:53:52 +0300

    Remove exclamation marks

---
+++ 8kyu/Remove exclation marks.js
@@ -0,0 +1,8 @@
+//❓ DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// ❗Solutions
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, "");
+  }
