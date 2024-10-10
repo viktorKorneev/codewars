85d25a057c12d6c4a7e95e3a74074c9235aacdaa
viktorKorneev <korneevviktor90@yandex.ru>
2024-10-10 05:06:36 +0300

    Contamination #1 -String-

---
+++ 8kyu/Contamination-1-String.js
@@ -0,0 +1,26 @@
+// ❓ DESCRIPTION:
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+// ❗ Solutions
+
+function contamination(text, char) {
+    let result = [];
+    text.split("").forEach(() => {
+      result.push(char);
+    });
+    return result.join("");
+  }
