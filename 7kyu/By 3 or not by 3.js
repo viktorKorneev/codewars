d0ec0c4c843aae1c2e75dcc368e571f7958ef116
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-02 00:48:20 +0300

    By 3, or not by 3? That is the question . . .

---
+++ 7kyu/By 3 or not by 3.js
@@ -0,0 +1,23 @@
+// ❓ Description:
+// A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+// Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+
+// Example:
+
+// "123"      -> true
+// "8409"     -> true
+// "100853"   -> false
+// "33333333" -> true
+// "7"        -> false
+// Try to avoid using the % (modulo) operator.
+
+// ❗ Solutions
+
+function divisibleByThree(str) {
+    let sum = 0;
+    str.split("").forEach((el) => {
+      sum += Number(el);
+    });
+    return sum % 3 === 0;
+  }
