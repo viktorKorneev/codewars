1a65d4c7ed0534f051ac96d51bf91af712e57376
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-10 16:09:18 +0300

    Grasshopper - Create the rooms

---
+++ 8kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,25 @@
+// ❓ Description:
+// Escape the room
+// You are creating an "Escape the room" game. The first step is to create a hash table 
+// ( dict in Python) called rooms that contains all of the rooms of the game. 
+// There should be at least 3 rooms inside it, each room being a hash table with at least 3 properties (e.g. name, description, completed).
+
+// ❗ Solutions
+
+var rooms = {
+    rooms1: {
+      name: 1,
+      description: 2,
+      completed: 3
+    },
+    rooms2: {
+      name: 1,
+      description: 2,
+      completed: 3
+    },
+    rooms3: {
+      name: 1,
+      description: 2,
+      completed: 3
+    }
+  }
