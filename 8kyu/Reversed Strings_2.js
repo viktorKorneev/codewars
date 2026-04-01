2c4820477358a91de87a63ffc7cbb3ad83e03ef7
viktorKorneev <korneevviktor90@yandex.ru>
2026-04-01 22:48:53 +0300

    Reverse Strings

---
+++ 8kyu/Reversed Strings_2.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str) {
+  let string = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    string += str[i];
+  }
+  return string;
+}
