953dfa6131efd73eee2e8cae3d70ecd42a9d3a0e
viktorKorneev <korneevviktor90@yandex.ru>
2026-04-27 21:38:19 +0300

    You only need one - Begineer

---
+++ 8kyu/You only nee one Begineer.js
@@ -0,0 +1,17 @@
+// ❓ DESCRIPTION:
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
+// ❗ Solutions
+
+function check(a, x) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === x) {
+      return true;
+    } 
+  }
+  return false
+}
