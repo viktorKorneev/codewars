45c1795c4bfc31c871a405f51849d23c0eb8a183
viktorKorneev <korneevviktor90@yandex.ru>
2026-08-12 00:27:54 +0300

    Remove an exclamation mark from the end of string

---
+++ 8kyu/Remove an from end of string.js
@@ -0,0 +1,16 @@
+//❓ DESCRIPTION:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+// ❗Solutions
+
+function remove (string) {
+  return string.charAt(string.length-1) === "!" ? string.slice(0, -1) : string;
+}
