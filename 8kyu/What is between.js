101265cd3f9c13604c509a815a268e8b1e04edcb
viktorKorneev <korneevviktor90@yandex.ru>
2026-08-15 00:35:33 +0300

    What is between?

---
+++ 8kyu/What is between.js
@@ -0,0 +1,19 @@
+// ❓ Description:
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+// ❗ Solutions
+
+
+function between(a, b) {
+  const arr = [];
+  for (let i = a; i <= b; i++) {
+    arr.push(i)
+  }
+  return arr;
+}
