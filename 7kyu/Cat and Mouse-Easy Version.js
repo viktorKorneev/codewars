535a4d32f4756e50687f42ca35b0b1cead591c95
viktorKorneev <korneevviktor90@yandex.ru>
2026-08-20 10:35:58 +0300

    Cat and Mouse-Easy Version

---
+++ 7kyu/Cat and Mouse-Easy Version.js
@@ -0,0 +1,15 @@
+// ❓ Description:
+
+// You will be given a string featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'. The string will start with the cat, and end with the mouse.
+
+// You need to find out if the cat can catch the mouse from its current position. The cat can jump over at most three characters. So:
+
+// "C.....m" returns "Escaped!" <-- more than three characters between
+
+// "C...m" returns "Caught!" <-- as there are three characters between the two, the cat can jump.
+
+// ❗ Solutions
+
+function catMouse(x){
+return x.length > 5 ? "Escaped!" : "Caught!"
+}
