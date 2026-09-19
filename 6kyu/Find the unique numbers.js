a0bbc8b71e09ddeba78f252e3ba526f6e028471f
viktorKorneev <korneevviktor90@yandex.ru>
2026-09-20 02:22:01 +0300

    Find the unique number

---
+++ 6kyu/Find the unique numbers.js
@@ -0,0 +1,24 @@
+// ❓ Description:
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// ❗ Solutions
+
+function findUniq(arr) {
+  const numbersObj = {}
+for(let i = 0; i < arr.length; i++){
+  numbersObj[arr[i]] = (numbersObj[arr[i]] || 0) + 1
+}
+console.log(numbersObj);
+
+for(const key in numbersObj) {
+  if(numbersObj[key] === 1) {
+    return Number(key)
+  }
+}
+}
